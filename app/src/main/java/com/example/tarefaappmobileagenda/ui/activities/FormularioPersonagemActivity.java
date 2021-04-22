1f02b6155d2cc8f676d119861f30e1aa762034ab
Uluskka <luska.pontes.freitas@hotmail.com>
2021-04-22 15:32:17 -0300

    Update 22/04/21

--- app/src/main/java/com/example/tarefaappmobileagenda/ui/activities/FormularioPersonagemActivity.java
+++ app/src/main/java/com/example/tarefaappmobileagenda/ui/activities/FormularioPersonagemActivity.java
@@ -2,15 +2,21 @@ package com.example.tarefaappmobileagenda.ui.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.telephony.ims.ImsMmTelManager;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.tarefaappmobileagenda.R;
 import com.example.tarefaappmobileagenda.dao.PersonagemDAO;
 import com.example.tarefaappmobileagenda.model.Personagem;
+import com.github.rtoshiro.util.format.SimpleMaskFormatter;
+import com.github.rtoshiro.util.format.text.MaskTextWatcher;
 
 import static com.example.tarefaappmobileagenda.ui.activities.ConstantesActivities.CHAVE_PERSONAGEM;
 
@@ -25,6 +31,21 @@ public class FormularioPersonagemActivity extends AppCompatActivity {
     private final PersonagemDAO dao = new PersonagemDAO();
     private Personagem personagem;
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_formulario_personagem_menu_salvar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == R.id.activity_formulario_personagem_menu_salvar) {
+            finalizarFormulario();
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -91,6 +112,14 @@ public class FormularioPersonagemActivity extends AppCompatActivity {
         campoNome = findViewById(R.id.edittext_nome);
         campoAltura = findViewById(R.id.edittext_altura);
         campoNascimento = findViewById(R.id.edittext_nascimento);
+        //identifca o campo para pode alterar.
+        SimpleMaskFormatter smfAltura = new SimpleMaskFormatter("N,NN");
+        MaskTextWatcher mtwAltura = new MaskTextWatcher(campoAltura, smfAltura);
+        campoAltura.addTextChangedListener(mtwAltura);
+
+        SimpleMaskFormatter smfNascimento = new SimpleMaskFormatter("NN/NN/NNNN");
+        MaskTextWatcher mtwNascimento = new MaskTextWatcher(campoNascimento, smfNascimento);
+        campoNascimento.addTextChangedListener(mtwNascimento);
     }
 
     private void preencherPersonagem() { //passa as informacoes digitas que voce deseja.
